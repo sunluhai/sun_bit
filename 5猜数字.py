013bb46c9a6a0b26c24eb4190ecd1ebe04479524
sunluhai <124947995+sunluhai@users.noreply.github.com>
2023-02-11 16:30:40 +0800

    Add files via upload

---
+++ 5猜数字.py
@@ -0,0 +1,15 @@
+num = 10
+if int(input("请输入第一次猜想的数字：")) == 10:
+    print("第一次猜对了")
+elif int(input("不对，再猜一次：")) == 10:
+    print("第二次猜对了")
+elif int(input("不对，再猜最后一次：")) == 10:
+    print("第三次猜对了")
+else:
+    print("Sorry,全部猜错了，我想的是：10")
+
+
+
+
+
+
---
+++ 6while_求1-100的和.py
@@ -0,0 +1,6 @@
+i = 1
+sum_1 = 0
+while i <= 100:
+    sum_1 += i
+    i += 1
+    print("1-100的和是：%d" % sum_1)
